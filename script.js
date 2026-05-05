ffb3881504ba14de2aec732646f897d4730eba02
Gak1o <gakiotoni@gmail.com>
2026-05-05 16:11:26 +0300

    Javascript lesson 1

---
+++ script.js
@@ -0,0 +1,66 @@
+/*function helloWorld(){
+
+ function val(){
+    let a = "Hello World!";
+    }
+
+    val();
+
+
+
+    //data types
+    let a = "Hello World!";
+    let b = 8;
+    let c = "!";
+    let d = 1.1;
+    let e = true;
+
+    //printing your data -- displaying your data 
+    //use console.log() to print your data to the console
+
+    console.log("A is",a, "B is",b, "C is",c, "D is",d, "E is",e);
+}
+//helloWorld();
+mathsClass();
+
+// arithmentic operations
+function mathsClass(){
+    let x = 10;
+    let y = 12;
+    let z = x+y;
+    let m = y-x;
+    let mx = y*x;
+    let dv = y/x;
+    let rm = y%x;
+
+    console.log("The sum is =", z,"The subtraction  is =", m, "The times is =", mx, "The division is =", dv, "The modulus is =", rm )
+}
+
+
+function addition(a,b){
+
+    return a+b;
+
+}
+console.log(addition(10, 12));
+
+//control flow 
+
+*/
+
+function checkAge(){
+    age = 20;
+
+    if(age >= 18){
+        console.log("Enter the site");
+
+}
+else {
+    console.log("Enter")
+}
+}
+
+checkAge();
+
+
+
